0f866a9e6fb7958fb0c1f0e7ab1a045639be9129
parshantsaini <sainiparshant1901@gmail.com>
2025-12-25 12:40:13 +0530

    plant detail

---
+++ frontend/src/components/StarRating.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Star } from "lucide-react";
+
+const StarRating = ({ rating = 0 }) => {
+  return (
+    <div className="flex">
+      {[...Array(5)].map((_, i) => (
+        <Star
+          key={i}
+          size={20}
+          className={
+            i < rating
+              ? "text-emerald-800 fill-emerald-800"
+              : "text-gray-300"
+          }
+        />
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
--- frontend/src/pages/PlantDetails.jsx
+++ frontend/src/pages/PlantDetails.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
+import React, { useState } from "react";
+import StarRating from "../components/StarRating";
+import { Droplet, Sun, TreePalm } from "lucide-react";
+
+const images = [
+  {
+    id: 1,
+    url: "/indoor_plants.jpg",
+  },
+  {
+    id: 2,
+    url: "/outdoor.jpg",
+  },
+  {
+    id: 3,
+    url: "/plant_pot.jpg",
+  },
+  {
+    id: 4,
+    url: "/indoor_plants.jpg",
+  },
+];
 
 const PlantDetails = () => {
-  return (
-    <div>
-      dsfjknfkjhfj
-    </div>
-  )
-}
+  const [activeImage, setActiveImage] = useState(images[0].url);
 
-export default PlantDetails
+  return (
+    <div className="min-h-screen">
+      <div className="flex p-10 gap-10">
+        <div className="max-w-xl ">
+          <div>
+            <img
+              src={activeImage}
+              alt=""
+              className="w-full rounded-lg duration-300"
+            />
+          </div>
+
+          <div className="grid grid-cols-4 gap-6 py-4 ">
+            {images.map((img) => (
+              <button
+                key={img.id}
+                onClick={() => setActiveImage(img.url)}
+                className={`rounded-lg border ${
+                  activeImage === img.url
+                    ? "border-emerald-500"
+                    : "border-gray-300"
+                }`}
+              >
+                <img src={img.url} alt="" className="rounded-lg cursor-pointer "/>
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <div className="flex gap-2">
+            <button className="bg-gray-200 p-1 text-xs rounded">Size</button>
+            <button className="bg-gray-200 p-1 text-xs rounded">Care</button>
+          </div>
+
+          <div className="mt-5 flex flex-col gap-3">
+            <h1 className="text-4xl font-semibold">Monte Carlo</h1>
+            <p className="p italic">plant category</p>
+
+            <div className="flex gap-2 items-center mt-5">
+              <StarRating rating={5}/>
+              <p className="p text-sm">(126 reviews)</p>
+            </div>
+
+            <div className="mt-5">
+              <h1 className="text-lg">
+                A bold plant with thick, glossy leaves in deep burgundy,
+                perfect for <br /> modern interiors.
+              </h1>
+            </div>
+          </div>
+
+          <hr className="mt-5 text-gray-300"/>
+
+          <div className="flex items-center justify-around mt-5">
+            <div className="flex flex-col gap-1 items-center">
+              <div className="bg-gray-300 rounded-full p-3">
+                <Sun className="text-emerald-800"/>
+              </div>
+              <h1 className="font-semibold text-md">Light</h1>
+              <p className="p text-sm font-medium">light me</p>
+            </div>
+            <div className="flex flex-col gap-1 items-center">
+              <div className="bg-gray-300 rounded-full p-3">
+                <Droplet className="text-emerald-800"/>
+              </div>
+              <h1 className="font-semibold text-md">Water</h1>
+              <p className="p text-sm font-medium">Medium</p>
+            </div>
+            <div className="flex flex-col gap-1 items-center">
+              <div className="bg-gray-300 rounded-full p-3">
+                <TreePalm className="text-emerald-800"/>
+              </div>
+              <h1 className="font-semibold text-md">Care Level</h1>
+              <p className="p text-sm font-medium">Easy</p>
+            </div>
+          </div>
+
+          <hr className="mt-5 text-gray-300"/>
+
+        </div>
+        
+
+      </div>
+    </div>
+  );
+};
+
+export default PlantDetails;
