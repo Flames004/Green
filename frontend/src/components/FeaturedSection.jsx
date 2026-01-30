8516ca09eb352fddba9617a4135153e3e340d26b
parshantsaini <sainiparshant1901@gmail.com>
2026-01-30 14:46:11 +0530

    feat: Enhance FeaturedSection and AddProduct components with validation and layout improvements

--- frontend/src/components/FeaturedSection.jsx
+++ frontend/src/components/FeaturedSection.jsx
@@ -15,7 +15,7 @@ const FeaturedSection = () => {
 
    const AllPlants = async()=>{
       try {
-        const {data} = await API.get("/products/all/plants?featured=true&available=true&limit=9");
+        const {data} = await API.get("/products/all/plants?featured=true&available=true&limit=8");
         setPlants(data.data.docs);
         
         
@@ -50,7 +50,7 @@ const FeaturedSection = () => {
         <p className='md:my-2 text-xs md:text-lg '>Discover our handpicked selection of vibrant plants.</p>
       </div>
 
-      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-6'>
+      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6'>
         {plants.map((plant) =>(
             <ProductCard key={plant._id} product={plant} productInfo={plant.plantDetails.category}/>
         ))}
